bb8731fb6bb5e5f6a36906c456ac892a615dd65f
RodrigooGuedes <rodrigogueedes7@gmail.com>
2026-03-17 17:54:00 -0300

    feat: Exceções pt1-pt3

---
+++ src/Oexception/exception/test/ExceptionTest01.java
@@ -0,0 +1,21 @@
+package Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("C:\\Users\\rodri\\OneDrive\\Documentos\\DevdojoVscode\\MaratonaJavaVscode\\arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked e Unchecked
+        int [] nums = {1,2};
+        System.out.println(nums[2]);
+    }
+}
