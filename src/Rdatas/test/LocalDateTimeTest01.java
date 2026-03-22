69f05fb2f54450e8156dd5664ce3e2031bf471cc
RodrigooGuedes <rodrigogueedes7@gmail.com>
2026-03-22 13:49:17 -0300

    feat: SimpleDateFormat, LocalDate, LocalTime, LocalDateTime

---
+++ src/Rdatas/test/LocalDateTimeTest01.java
@@ -0,0 +1,22 @@
+package Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localdatetime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2022-08-06");
+        LocalTime time = LocalTime.parse("09:45:00");
+        System.out.println(localdatetime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime ltd1 = date.atTime(time);
+        LocalDateTime ltd2 = time.atDate(date);
+        System.out.println(ltd1);
+        System.out.println(ltd2);
+    
+    }
+}
---
+++ src/Rdatas/test/LocalTimeTest01.java
@@ -0,0 +1,20 @@
+package Rdatas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        LocalTime timeagora = LocalTime.now();
+
+        System.out.println(time);
+        System.out.println(timeagora);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.CLOCK_HOUR_OF_AMPM));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
+    }
+}
